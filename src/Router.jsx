142df809bca30aa5195f9a0fb10747bfb74022ef
Ashik-Himel <ashikujjamanhimel@gmail.com>
2023-10-16 23:36:57 +0600

    server api link updated localhost to vercel

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,35 +1,41 @@
-import { createBrowserRouter } from 'react-router-dom';
-import Layout from './Layout';
-import Home from './pages/Home';
-import AddCoffee from './pages/AddCoffee';
-import CoffeeDetails from './pages/CoffeeDetails';
-import UpdateCoffee from './pages/UpdateCoffee';
-import ErrorPage from './pages/ErrorPage';
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+import AddCoffee from "./pages/AddCoffee";
+import CoffeeDetails from "./pages/CoffeeDetails";
+import UpdateCoffee from "./pages/UpdateCoffee";
+import ErrorPage from "./pages/ErrorPage";
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Layout />,
     errorElement: <ErrorPage />,
     children: [
       {
-        path: '/',
-        element: <Home />
+        path: "/",
+        element: <Home />,
       },
       {
-        path: '/add',
-        element: <AddCoffee />
+        path: "/add",
+        element: <AddCoffee />,
       },
       {
-        path: '/update/:id',
+        path: "/update/:id",
         element: <UpdateCoffee />,
-        loader: ({params}) => fetch(`http://localhost:5001/coffees/${params.id}`)
+        loader: ({ params }) =>
+          fetch(
+            `https://espresso-emporium-server-side.vercel.app/coffees/${params.id}`
+          ),
       },
       {
-        path: '/coffees/:id',
+        path: "/coffees/:id",
         element: <CoffeeDetails />,
-        loader: ({params}) => fetch(`http://localhost:5001/coffees/${params.id}`)
-      }
-    ]
-  }
-])
+        loader: ({ params }) =>
+          fetch(
+            `https://espresso-emporium-server-side.vercel.app/coffees/${params.id}`
+          ),
+      },
+    ],
+  },
+]);
--- src/pages/AddCoffee.jsx
+++ src/pages/AddCoffee.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
-import Swal from 'sweetalert2'
-import { BsArrowLeft } from 'react-icons/bs';
+import Swal from "sweetalert2";
+import { BsArrowLeft } from "react-icons/bs";
 
 const AddCoffee = () => {
-  const handleAdd = async(e) => {
+  const handleAdd = async (e) => {
     e.preventDefault();
 
     const name = e.target.name.value;
@@ -14,77 +14,206 @@ const AddCoffee = () => {
     const details = e.target.details.value;
     const price = e.target.price.value;
     const photo = e.target.photo.value;
-    const newCoffee = {name, chef, supplier, taste, category, details, price, photo}
-    
-    const res = await fetch('http://localhost:5001/coffees', {
-      method: "POST",
-      headers: {
-        "content-type": "application/json"
-      },
-      body: JSON.stringify(newCoffee)
-    });
+    const newCoffee = {
+      name,
+      chef,
+      supplier,
+      taste,
+      category,
+      details,
+      price,
+      photo,
+    };
+
+    const res = await fetch(
+      "https://espresso-emporium-server-side.vercel.app/coffees",
+      {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(newCoffee),
+      }
+    );
     const data = await res.json();
     if (data.insertedId) {
       Swal.fire({
-        title: 'Successful !!!',
-        text: 'Coffee added successfully!',
-        icon: 'success',
-        confirmButtonText: 'Close'
+        title: "Successful !!!",
+        text: "Coffee added successfully!",
+        icon: "success",
+        confirmButtonText: "Close",
       });
       e.target.reset();
     }
-  }
+  };
 
   return (
     <main className="mt-8 mb-16">
       <div className="container">
-        <Link to='/' className="btn bg-[transparent] border-none hover:bg-primary !rounded-md mb-6" style={{textShadow: '1px 1px 10px'}}><BsArrowLeft /> Back to Home</Link>
+        <Link
+          to="/"
+          className="btn bg-[transparent] border-none hover:bg-primary !rounded-md mb-6"
+          style={{ textShadow: "1px 1px 10px" }}
+        >
+          <BsArrowLeft /> Back to Home
+        </Link>
         <div className="bg-bg-color p-10 rounded-lg">
-          <h2 className="text-3xl text-center mb-2" style={{textShadow: '2px 2px 5px'}}>Add New Coffee</h2>
-          <p className="max-w-[550px] mx-auto text-center mb-8 opacity-70">It is a long established fact that a reader will be destructed by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.</p>
+          <h2
+            className="text-3xl text-center mb-2"
+            style={{ textShadow: "2px 2px 5px" }}
+          >
+            Add New Coffee
+          </h2>
+          <p className="max-w-[550px] mx-auto text-center mb-8 opacity-70">
+            It is a long established fact that a reader will be destructed by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsum is that it has a more-or-less normal
+            distribution of letters, as opposed to using Content here.
+          </p>
 
           <form onSubmit={handleAdd}>
             <div className="flex flex-col md:flex-row justify-center items-center gap-4 mb-4">
               <div className="w-full">
-                <label className="font-raleway font-bold mb-2 block" htmlFor="name">Name</label>
-                <input className="input w-full font-raleway focus:border-primary focus:[box-shadow:0px_0px_4px_1px_#331A15]" type="text" name="name" id="name" placeholder="Enter coffee name" required />
+                <label
+                  className="font-raleway font-bold mb-2 block"
+                  htmlFor="name"
+                >
+                  Name
+                </label>
+                <input
+                  className="input w-full font-raleway focus:border-primary focus:[box-shadow:0px_0px_4px_1px_#331A15]"
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Enter coffee name"
+                  required
+                />
               </div>
               <div className="w-full">
-                <label className="font-raleway font-bold mb-2 block" htmlFor="chef">Chef</label>
-                <input className="input w-full font-raleway focus:border-primary focus:[box-shadow:0px_0px_4px_1px_#331A15]" type="text" name="chef" id="chef" placeholder="Enter coffee chef" required />
+                <label
+                  className="font-raleway font-bold mb-2 block"
+                  htmlFor="chef"
+                >
+                  Chef
+                </label>
+                <input
+                  className="input w-full font-raleway focus:border-primary focus:[box-shadow:0px_0px_4px_1px_#331A15]"
+                  type="text"
+                  name="chef"
+                  id="chef"
+                  placeholder="Enter coffee chef"
+                  required
+                />
               </div>
             </div>
             <div className="flex flex-col md:flex-row justify-center items-center gap-4 mb-4">
               <div className="w-full">
-                <label className="font-raleway font-bold mb-2 block" htmlFor="supplier">Supplier</label>
-                <input className="input w-full font-raleway focus:border-primary focus:[box-shadow:0px_0px_4px_1px_#331A15]" type="text" name="supplier" id="supplier" placeholder="Enter coffee supplier" required />
+                <label
+                  className="font-raleway font-bold mb-2 block"
+                  htmlFor="supplier"
+                >
+                  Supplier
+                </label>
+                <input
+                  className="input w-full font-raleway focus:border-primary focus:[box-shadow:0px_0px_4px_1px_#331A15]"
+                  type="text"
+                  name="supplier"
+                  id="supplier"
+                  placeholder="Enter coffee supplier"
+                  required
+                />
               </div>
               <div className="w-full">
-                <label className="font-raleway font-bold mb-2 block" htmlFor="taste">Taste</label>
-                <input className="input w-full font-raleway focus:border-primary focus:[box-shadow:0px_0px_4px_1px_#331A15]" type="text" name="taste" id="taste" placeholder="Enter coffee taste" required />
+                <label
+                  className="font-raleway font-bold mb-2 block"
+                  htmlFor="taste"
+                >
+                  Taste
+                </label>
+                <input
+                  className="input w-full font-raleway focus:border-primary focus:[box-shadow:0px_0px_4px_1px_#331A15]"
+                  type="text"
+                  name="taste"
+                  id="taste"
+                  placeholder="Enter coffee taste"
+                  required
+                />
               </div>
             </div>
             <div className="flex flex-col md:flex-row justify-center items-center gap-4 mb-4">
               <div className="w-full">
-                <label className="font-raleway font-bold mb-2 block" htmlFor="category">Category</label>
-                <input className="input w-full font-raleway focus:border-primary focus:[box-shadow:0px_0px_4px_1px_#331A15]" type="text" name="category" id="category" placeholder="Enter coffee category" required />
+                <label
+                  className="font-raleway font-bold mb-2 block"
+                  htmlFor="category"
+                >
+                  Category
+                </label>
+                <input
+                  className="input w-full font-raleway focus:border-primary focus:[box-shadow:0px_0px_4px_1px_#331A15]"
+                  type="text"
+                  name="category"
+                  id="category"
+                  placeholder="Enter coffee category"
+                  required
+                />
               </div>
               <div className="w-full">
-                <label className="font-raleway font-bold mb-2 block" htmlFor="details">Details</label>
-                <input className="input w-full font-raleway focus:border-primary focus:[box-shadow:0px_0px_4px_1px_#331A15]" type="text" name="details" id="details" placeholder="Enter coffee details" required />
+                <label
+                  className="font-raleway font-bold mb-2 block"
+                  htmlFor="details"
+                >
+                  Details
+                </label>
+                <input
+                  className="input w-full font-raleway focus:border-primary focus:[box-shadow:0px_0px_4px_1px_#331A15]"
+                  type="text"
+                  name="details"
+                  id="details"
+                  placeholder="Enter coffee details"
+                  required
+                />
               </div>
             </div>
             <div className="flex flex-col md:flex-row justify-center items-center gap-4 mb-6">
               <div className="w-full">
-                <label className="font-raleway font-bold mb-2 block" htmlFor="price">Price</label>
-                <input className="input w-full font-raleway focus:border-primary focus:[box-shadow:0px_0px_4px_1px_#331A15]" type="text" name="price" id="price" placeholder="Enter coffee price" required />
+                <label
+                  className="font-raleway font-bold mb-2 block"
+                  htmlFor="price"
+                >
+                  Price
+                </label>
+                <input
+                  className="input w-full font-raleway focus:border-primary focus:[box-shadow:0px_0px_4px_1px_#331A15]"
+                  type="text"
+                  name="price"
+                  id="price"
+                  placeholder="Enter coffee price"
+                  required
+                />
               </div>
               <div className="w-full">
-                <label className="font-raleway font-bold mb-2 block" htmlFor="photo">Photo</label>
-                <input className="input w-full font-raleway focus:border-primary focus:[box-shadow:0px_0px_4px_1px_#331A15]" type="text" name="photo" id="photo" placeholder="Enter photo URL" required />
+                <label
+                  className="font-raleway font-bold mb-2 block"
+                  htmlFor="photo"
+                >
+                  Photo
+                </label>
+                <input
+                  className="input w-full font-raleway focus:border-primary focus:[box-shadow:0px_0px_4px_1px_#331A15]"
+                  type="text"
+                  name="photo"
+                  id="photo"
+                  placeholder="Enter photo URL"
+                  required
+                />
               </div>
             </div>
-            <button type="submit" className="btn btn-primary border-secondary btn-block">Add Coffee</button>
+            <button
+              type="submit"
+              className="btn btn-primary border-secondary btn-block"
+            >
+              Add Coffee
+            </button>
           </form>
         </div>
       </div>
